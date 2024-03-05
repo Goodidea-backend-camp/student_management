415680e9b3c8df97a10c45887584ef57c3834628
devusexu <devusexu@gmail.com>
2024-03-05 20:56:55 +0800

    refactor: switch from $guarded to $fillable for explicit attribute definition

--- app/Models/RegistrationToken.php
+++ app/Models/RegistrationToken.php
@@ -10,7 +10,7 @@ class RegistrationToken extends Model
 {
     use HasFactory;
 
-    protected $guarded = [];
+    protected $fillable = ['proposed_username', 'value', 'expired_time'];
     public function user(): belongsTo
     {
        return $this->belongsTo(User::class);
