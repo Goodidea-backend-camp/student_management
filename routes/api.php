1382db79bb3bbef1cb04d284d032bb1fc5967227
devusexu <devusexu@gmail.com>
2024-03-06 14:07:13 +0800

    refactor: remove unused route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\StudentController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -15,10 +14,6 @@ use App\Http\Controllers\AuthController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::post('register', [StudentController::class, 'create']);
 Route::post('login', [AuthController::class, 'login']);
