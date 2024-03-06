8ed8c17b498db7511a8d325609d59eaa62439486
devusexu <devusexu@gmail.com>
2024-03-06 10:31:41 +0800

    refactor: remove unused route

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/tokens', [RegistrationTokenController::class, 'index']);
 Route::post('/tokens', [RegistrationTokenController::class, 'store']);
 
